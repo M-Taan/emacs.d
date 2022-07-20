eeb344990da3a701158386b4ef38f13c148c2a50
Mohamad Taan <mohamadmtaan@gmail.com>
2022-07-20 13:53:44 +0300

    some init.el fixes

--- init.el
+++ init.el
@@ -31,35 +31,48 @@
   '(;; makes handling lisp expressions much, much easier
     ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
     paredit
+
     flycheck
     ;; key bindings and code colorization for Clojure
     ;; https://github.com/clojure-emacs/clojure-mode
-    clojure-mode 
+    clojure-mode
+
     ;; extra syntax highlighting for clojure
     clojure-mode-extra-font-locking
+
     flycheck-clj-kondo
+
     ;; integration with a Clojure REPL
     ;; https://github.com/clojure-emacs/cider
     cider
+
     ;; Enhances M-x to allow easier execution of commands. Provides
     ;; a filterable list of possible commands in the minibuffer
     ;; http://www.emacswiki.org/emacs/Smex
     smex
+
     ;; project navigation
     projectile
+
     ;; Javascript and Web in general related stuff
     ;; we need to move this stuff to other
     ;; file (when I have time to play around it on the weekend)
     web-mode
+
     prettier-js
+
     ;; python related stuff
     elpy
+
     ;; edit html tags like sexps
     tagedit
+
     ;; git integration
     magit
+
     ;; for copy/paste support between the terminal and the window system
     xclip
+
     ;; solarized-theme (my fav :$)
     solarized-theme))
 
@@ -134,7 +147,9 @@
  '(coffee-tab-width 4)
  '(custom-enabled-themes '(solarized-selenized-black))
  '(custom-safe-themes
-   '("1d0585cda7392ea307d2456bd444e7b06eabb3fdb95395e16ac6a36901e4d03d" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" default)))
+   '("1d0585cda7392ea307d2456bd444e7b06eabb3fdb95395e16ac6a36901e4d03d" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" default))
+ '(package-selected-packages
+   '(solarized-theme xclip magit tagedit elpy prettier-js web-mode projectile smex cider flycheck-clj-kondo clojure-mode-extra-font-locking clojure-mode flycheck paredit)))
 
  
 (custom-set-faces
