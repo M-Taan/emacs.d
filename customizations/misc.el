098232a9740f1392f9020a209c17a2d50014184c
Mohamad Taan <mohamadmtaan@gmail.com>
2022-09-04 16:46:08 +0300

    closes #3, hook for grep-mode and custom functions for splitting below and right

--- customizations/misc.el
+++ customizations/misc.el
@@ -13,3 +13,29 @@
 
 ;; run commands fast
 (global-set-key (kbd "C-c c") 'counsel-compile)
+
+;; everytime any buffer uses grep-mode
+;; emacs will automatically switch to that buffer
+(add-hook 'grep-mode-hook
+          '(lambda ()
+             (switch-to-buffer-other-window "*grep*")))
+
+(defun split-window-and-switch-below ()
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+
+;; bind to the split-horzn function
+(global-set-key (kbd "C-x 2") 'split-window-and-switch-below)
+
+
+(defun split-window-and-switch-right ()
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+
+;; bind to the split-vert function
+(global-set-key (kbd "C-x 3") 'split-window-and-switch-right)
+
