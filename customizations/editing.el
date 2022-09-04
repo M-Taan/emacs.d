15df6da8ba4adac24ffac1bea5214ff315618e4e
Mohamad Taan <mohamadmtaan@gmail.com>
2022-09-04 18:46:22 +0300

    closes #6, swiper instead of isearch

--- customizations/editing.el
+++ customizations/editing.el
@@ -6,12 +6,11 @@
 ;; Highlight current line
 (global-hl-line-mode 1)
 
-;; Interactive search key bindings. By default, C-s runs
-;; isearch-forward, so this swaps the bindings.
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
+;; swiper instead of isearch
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-M-a") 'swiper-all)
+(global-set-key (kbd "C-M-s") 'swiper-isearch)
+(global-set-key (kbd "C-M-r") 'swiper-isearch-backward)
 
 ;; Don't use hard tabs
 (setq-default indent-tabs-mode nil)
