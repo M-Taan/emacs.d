6e24a1b317aa552dc256962c07b0b1547a5300f8
Mohamad Taan <mohamadmtaan@gmail.com>
2022-09-16 10:46:53 +0300

    updated swiper
    updated swiper

--- customizations/editing.el
+++ customizations/editing.el
@@ -6,11 +6,8 @@
 ;; Highlight current line
 (global-hl-line-mode 1)
 
-;; swiper instead of isearch
-(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-s") 'swiper-isearch)
 (global-set-key (kbd "C-M-a") 'swiper-all)
-(global-set-key (kbd "C-M-s") 'swiper-isearch)
-(global-set-key (kbd "C-M-r") 'swiper-isearch-backward)
 
 ;; Don't use hard tabs
 (setq-default indent-tabs-mode nil)
