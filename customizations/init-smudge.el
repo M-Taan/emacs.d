4388f506288c9ab8716fc67a582b355cc939da66
Mohamad Taan <mohamadmtaan@gmail.com>
2023-10-08 14:27:19 +0300

    feat: re-introduced smudge to the config

---
+++ customizations/init-smudge.el
@@ -0,0 +1,10 @@
+(use-package smudge
+  :ensure t
+  :after simple-httpd
+  :init
+  (setq smudge-oauth2-client-secret (getenv "SPOTIFY_SECRET"))
+  (setq smudge-oauth2-client-id (getenv "SPOTIFY_CLIENT"))
+  (setq smudge-transport 'connect)
+  :bind
+  ("C-c ." . smudge-command-map)
+  ("C-c s" . global-smudge-remote-mode))
